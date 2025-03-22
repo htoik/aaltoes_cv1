09b3e172abf37e130ceb9f2127a8b8380f13351c
htoik <henrik.toikka@aalto.fi>
2025-03-22 15:56:14 +0200

    dataset update

--- utils/datasets.py
+++ utils/datasets.py
@@ -6,50 +6,59 @@ class AaltoesDataset(Dataset):
     def __init__(self, root_dir, mode="train", transform=None):
         self.mode = mode
         self.transform = transform
+        self.include_full_forged = True
+        self.dataset = {}
 
         if mode == "train":
             self.image_dir = os.path.join(root_dir, "train", "train", "images")
             self.mask_dir = os.path.join(root_dir, "train", "train", "masks")
             self.orig_dir = os.path.join(root_dir, "train", "train", "originals")
-            self.image_files = sorted(os.listdir(self.image_dir))
-            mask_files = sorted(os.listdir(self.image_dir))
-            orig_files = sorted(os.listdir(self.image_dir))
-            self.max_idx = min([len(self.image_files), len(mask_files), len(orig_files)])
+
+            dict1 = [(int(img.split('_')[1].split('.')[0]), img) for img in self.image_dir]
+            dict2 = [(int(img.split('_')[1].split('.')[0]), img) for img in self.mask_dir]
+            dict3 = [(int(img.split('_')[1].split('.')[0]), img) for img in self.orig_dir]
+
+            ks = set(dict1.keys()).add(set(dict2.keys(()))).add(set(dict3.keys(())))
+            self.dataset = dict((k, (None, None, None)) for k in ks)
+            for idx, img in dict1: self.dataset[idx][0] = img
+            for idx, img in dict2: self.dataset[idx][1] = img
+            for idx, img in dict3: self.dataset[idx][2] = img
         elif mode == "test":
             self.image_dir = os.path.join(root_dir, "test", "test", "images")
-            self.image_files = sorted(os.listdir(self.image_dir))
-            self.max_idx = len(self.image_files)
+            # self.image_files = sorted(os.listdir(self.image_dir))
+            dict1 = [(int(img.split('_')[1].split('.')[0]), img) for img in self.image_dir]
+            ks = set(dict1.keys()).add(set(dict2.keys(()))).add(set(dict3.keys(())))
+
+            self.dataset = dict((k, (None, None, None)) for k in ks)
+            for idx, img in dict1: self.dataset[idx][0] = img
         else:
             raise ValueError("Mode must be 'train' or 'test'")
 
     def __len__(self):
-        # return len(self.image_files)
-        return self.max_idx
+        return len(self.dataset)
 
     def __getitem__(self, idx):
-        img_name = self.image_files[idx]
+        img_name, mask_name, orig_name = self.dataset[idx]
         image_path = os.path.join(self.image_dir, img_name)
 
         image = Image.open(image_path).convert("RGB")
 
         if self.mode == "train":
-            mask_path = os.path.join(self.mask_dir, img_name)
-            orig_path = os.path.join(self.orig_dir, img_name)
-
+            mask_path = os.path.join(self.mask_dir, mask_name)
             mask = Image.open(mask_path).convert("L")
+
             try:
+                orig_path = os.path.join(self.orig_dir, orig_name)
                 original = Image.open(orig_path).convert("RGB")
             except FileNotFoundError:
-                original = None
+                original = Image.new('RGB', image.size[1::-1])
 
             if self.transform:
                 image = self.transform(image)
                 mask = self.transform(mask)
-                if original is not None:
-                    original = self.transform(original)
+                original = self.transform(original)
 
-            sample = {"image": image, "mask": mask}
-            # sample = {"image": image, "mask": mask, "original": original}
+            sample = {"image": image, "mask": mask, "original": original}
         else:
             if self.transform:
                 image = self.transform(image)
